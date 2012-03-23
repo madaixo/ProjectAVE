626a5adca2f8cdea32ca10efed9fc7ff3669b519
Nuno Bento <ngpbento@gmail.com>
2012-03-23 18:57:14 +0000

    Removed old import

--- halfnes-0.045/Run Me For Controller Support.bat
+++
@@ -1,9 +0,0 @@
-@ECHO OFF
-echo Starting HalfNES...
-java -Djava.library.path=./lib -jar HalfNES.jar %1
-IF NOT EXIST .\lib\jinput.jar GOTO NoLibs
-GOTO EOF
-:NoLibs
-echo HalfNES cannot start because the library folder is missing.
-pause
-:EOF
